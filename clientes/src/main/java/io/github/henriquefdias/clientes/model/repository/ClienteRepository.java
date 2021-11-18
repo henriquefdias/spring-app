e946af334e29e7fefa42e4aaac17a3068b6ad7db
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-11-18 20:58:27 -0300

    Initial commit

---
+++ clientes/src/main/java/io/github/henriquefdias/clientes/model/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package io.github.henriquefdias.clientes.model.repository;
+
+import io.github.henriquefdias.clientes.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
