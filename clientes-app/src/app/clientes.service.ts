dc4a2cc7d0338567c029a80ee1a3cac7e407d864
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-12-08 17:28:11 -0300

    Criação da listagem de clientes

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -14,11 +14,13 @@ export class ClientesService {
   salvar(cliente: Cliente) : Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
-  
-  getCliente() : Cliente {
-    let cliente: Cliente = new Cliente();
-    cliente.nome = 'Fulano';
-    cliente.cpf = '88888888888';
-    return cliente;
+
+  getClientes() : Cliente[] {
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = "Fulano";
+    cliente.dataCadastro = '08/12/2021'
+    cliente.cpf = '12345678901'
+    return [cliente];
   }
 }
