f29c42667083c368d62d6be051d7a7e9a122f1e5
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-12-23 09:06:52 -0300

    Criação do service de Serviço Prestado

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ClientesModule } from './clientes/clientes.module';
 import { ClientesService } from "./clientes.service";
 import { HttpClientModule } from "@angular/common/http";
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { ServicoPrestadoService } from './servico-prestado.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.modul
     ServicoPrestadoModule
   ],
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService
   ],
   bootstrap: [AppComponent]
 })
