002b8f581e155016c55975089aae224e63b1da85
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-01-02 11:05:57 -0300

    Refatoração da exibição do template

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
-
-declare var $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent{
   title = 'clientes-app';
-
-  ngAfterViewInit(){
-    var path = location.pathname;
-    
-    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index:any, element:any) => {
-        if ($(element).attr('href') === path) {
-            $(element).addClass("active");
-        }
-    });
-     
-    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-  }
 }
---
+++ clientes-app/src/app/layout/layout.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    var path = location.pathname;
+    
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index:any, element:any) => {
+        if ($(element).attr('href') === path) {
+            $(element).addClass("active");
+        }
+    });
+     
+    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+  }
+
+}
