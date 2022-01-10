6806e423898dde79758179c417dd891a785bca1a
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-01-10 09:05:49 -0300

    Criação do Password Encoder

--- clientes/src/main/java/io/github/henriquefdias/clientes/config/SecurityConfig.java
+++ clientes/src/main/java/io/github/henriquefdias/clientes/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -34,5 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .cors()
         .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
