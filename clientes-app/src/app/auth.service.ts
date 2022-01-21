09d91fc99234809914567723a30dc4845b795859
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-01-21 10:49:11 -0300

    Validação de expiração de token e redirecionamento para tela de login

--- clientes-app/src/app/auth.service.ts
+++ clientes-app/src/app/auth.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Usuario } from './login/usuario';
 
 import { environment } from "../environments/environment";
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +15,27 @@ export class AuthService {
   tokenURL: string = environment.apiURLBase + environment.obterTokenUrl
   clientID: string = environment.clientId
   clientSecret: string = environment.clientSecret
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(
     private http: HttpClient
   ) { }
 
+  obterToken() {
+    const tokenString = localStorage.getItem('access_token');
+    if (tokenString) {
+      const token = JSON.parse(tokenString).access_token
+      return token;
+    }
+    return null;
+  }
+
   isAuthenticated(): boolean {
+    const token = this.obterToken();
+    if(token){
+      const expired = this.jwtHelper.isTokenExpired(token)
+      return !expired;
+    }
     return false;
   }
 
