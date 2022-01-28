f0bc8b53b721d1e6ce3014f7a82f5423476d8c8c
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-01-28 17:12:31 -0300

    Adição do componente e service de Contato

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
@@ -16,7 +19,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
