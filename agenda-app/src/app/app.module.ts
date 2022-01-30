6558f2e0f8bfbf0f739616a5de0a1db4e3d3a29b
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-01-30 17:52:42 -0300

    Conectando front end à API

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  HttpClientModule } from "@angular/common/http";
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -19,6 +20,7 @@ import { ContatoService } from './contato.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     MatButtonModule,
     MatInputModule,
     MatToolbarModule,
