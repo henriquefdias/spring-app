5d59608543deab4d5ffa5d026c173c15fd9cf374
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-02-15 21:28:48 -0300

    Paginação no front end

--- agenda-app/src/app/contato/contato.component.ts
+++ agenda-app/src/app/contato/contato.component.ts
@@ -5,6 +5,7 @@ import { Contato } from './contato';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-contato',
@@ -16,6 +17,11 @@ export class ContatoComponent implements OnInit {
   formulario: FormGroup;
   contatos: Contato[] = [];
   colunas: String[] = ['foto', 'id', 'nome', 'email', 'favorito'];
+
+  totalElementos: number = 0;
+  pagina: number = 0;
+  tamanho: number = 1;
+  pageSizeOptions: number[] = [1]
 
   constructor(
     private service: ContatoService,
@@ -29,16 +35,18 @@ export class ContatoComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    this.listarContatos();
+    this.listarContatos(this.pagina, this.tamanho);
   }
   
-  listarContatos(): void {
-    this.service.list().subscribe(response => {
-      this.contatos = response;
+  listarContatos(pagina: any = 0, tamanho: any = 10): void {
+    this.service.list(pagina, tamanho).subscribe(response => {
+      this.contatos = response.content;
+      this.totalElementos = response.totalElements;
+      this.pagina = response.number;
     })
   }
 
-  favoritar(contato: Contato): void{
+  favoritar(contato: Contato): void {
     this.service.favourite(contato).subscribe(response => {
       contato.favorito = !contato.favorito;
     })
@@ -72,6 +80,11 @@ export class ContatoComponent implements OnInit {
       height: '450px',
       data: contato
     })
+  }
+
+  paginar(event: PageEvent): void {
+    this.pagina = event.pageIndex;
+    this.listarContatos(this.pagina, this.tamanho)
   }
 
 }
